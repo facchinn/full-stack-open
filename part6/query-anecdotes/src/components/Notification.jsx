59fed172729987bf8393ed1daeab480d7d436f4f
facchinn <nazafacchin@hotmail.com>
2026-08-20 17:31:32 -0300

    add notification component

---
+++ part6/query-anecdotes/src/components/Notification.jsx
@@ -0,0 +1,15 @@
+import { useNotification } from '../NotificationContext'
+
+const Notification = () => {
+  const notification = useNotification()
+
+  if (!notification) return null
+
+  return (
+    <div style={{ border: 'solid', padding: 10, marginBottom: 10 }}>
+      {notification}
+    </div>
+  )
+}
+
+export default Notification
