354689a270fcec757fd598bb56321aaa08744c54
facchinn <nazafacchin@hotmail.com>
2026-08-20 18:23:30 -0300

    extract persistent user service

---
+++ part7/bloglist/frontend/src/services/persistentUser.js
@@ -0,0 +1,23 @@
+const storageKey = 'loggedBlogappUser'
+
+const getUser = () => {
+  const savedUser = window.localStorage.getItem(storageKey)
+  if (!savedUser) return null
+
+  try {
+    return JSON.parse(savedUser)
+  } catch {
+    window.localStorage.removeItem(storageKey)
+    return null
+  }
+}
+
+const saveUser = user => {
+  window.localStorage.setItem(storageKey, JSON.stringify(user))
+}
+
+const removeUser = () => {
+  window.localStorage.removeItem(storageKey)
+}
+
+export default { getUser, saveUser, removeUser }
