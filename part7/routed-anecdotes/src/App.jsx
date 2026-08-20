4f1abcfaa14dcd81b25ae9a6c79fd994abaa72e7
facchinn <nazafacchin@hotmail.com>
2026-08-20 18:11:56 -0300

    complete routed anecdotes app

---
+++ part7/routed-anecdotes/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import About from './components/About'
+import AnecdoteList from './components/AnecdoteList'
+import CreateNew from './components/CreateNew'
+import Footer from './components/Footer'
+import Menu from './components/Menu'
+
+const App = () => (
+  <Router>
+    <div>
+      <h1>Software anecdotes</h1>
+      <Menu />
+      <Routes>
+        <Route path="/" element={<AnecdoteList />} />
+        <Route path="/create" element={<CreateNew />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <Footer />
+    </div>
+  </Router>
+)
+
+export default App
