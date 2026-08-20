65fb02036bf05821c30103661ad266bcd24f59e5
facchinn <nazafacchin@hotmail.com>
2026-08-20 18:27:24 -0300

    add comments and blog operations to combined backend

---
+++ part7/bloglist/backend/controllers/blogs.js
@@ -0,0 +1,101 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+  response.json(blogs)
+})
+
+blogsRouter.post('/', async (request, response) => {
+  const { title, author, url, likes } = request.body
+  const user = request.user
+
+  if (!user) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  if (!title || !url) {
+    return response.status(400).json({ error: 'title and url are required' })
+  }
+
+  const blog = new Blog({
+    title,
+    author,
+    url,
+    likes,
+    user: user._id,
+  })
+
+  const savedBlog = await blog.save()
+  user.blogs = user.blogs.concat(savedBlog._id)
+  await user.save()
+  await savedBlog.populate('user', { username: 1, name: 1 })
+
+  response.status(201).json(savedBlog)
+})
+
+blogsRouter.post('/:id/comments', async (request, response) => {
+  const comment = request.body.comment?.trim()
+
+  if (!comment) {
+    return response.status(400).json({ error: 'comment is required' })
+  }
+
+  const blog = await Blog.findById(request.params.id)
+  if (!blog) {
+    return response.status(404).end()
+  }
+
+  blog.comments = blog.comments.concat(comment)
+  const savedBlog = await blog.save()
+  await savedBlog.populate('user', { username: 1, name: 1 })
+
+  response.status(201).json(savedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  const user = request.user
+
+  if (!user) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
+
+  const blog = await Blog.findById(request.params.id)
+  if (!blog) {
+    return response.status(404).end()
+  }
+
+  if (!blog.user || blog.user.toString() !== user._id.toString()) {
+    return response
+      .status(403)
+      .json({ error: 'only the creator can delete this blog' })
+  }
+
+  await Blog.findByIdAndDelete(request.params.id)
+  user.blogs = user.blogs.filter(id => id.toString() !== blog._id.toString())
+  await user.save()
+
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const { title, author, url, likes } = request.body
+
+  const updatedBlog = await Blog.findByIdAndUpdate(
+    request.params.id,
+    { title, author, url, likes },
+    {
+      returnDocument: 'after',
+      runValidators: true,
+      context: 'query',
+    }
+  ).populate('user', { username: 1, name: 1 })
+
+  if (!updatedBlog) {
+    return response.status(404).end()
+  }
+
+  response.json(updatedBlog)
+})
+
+module.exports = blogsRouter
