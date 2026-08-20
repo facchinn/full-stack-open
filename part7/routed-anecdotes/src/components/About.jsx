030e1a64d61986d27407ceec62bc52807f1bb05e
facchinn <nazafacchin@hotmail.com>
2026-08-20 18:13:01 -0300

    add routed anecdotes about page

---
+++ part7/routed-anecdotes/src/components/About.jsx
@@ -0,0 +1,14 @@
+const About = () => (
+  <div>
+    <h2>About anecdote app</h2>
+    <p>
+      According to Wikipedia, software engineering is the systematic application
+      of engineering approaches to the development of software.
+    </p>
+    <p>
+      This app collects short software development anecdotes.
+    </p>
+  </div>
+)
+
+export default About
